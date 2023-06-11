3b5292397364789b4c6a6e75b200a68532cbe68a
vstarvrajesh <109358855+vstarvrajesh@users.noreply.github.com>
2023-06-11 17:32:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  setTimeout(() => {
+    let string = "";
+    let buttons = document.querySelectorAll('.button');
+    Array.from(buttons).forEach((button) => {
+      button.addEventListener('click', (e) => {
+        if (e.target.innerHTML === '=') {
+          let a = eval(string);
+          string = a.toString();
+          document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML === 'C') {
+          string = ""
+          document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML === 'del') {
+          console.log(string)
+          string = string.replace(/.$/, '');
+          document.querySelector('input').value = string;
+        }
+        else {
+          console.log(e.target)
+          string = string + e.target.innerHTML;
+          document.querySelector('input').value = string;
+        }
+      })
+    })
+  }, 100);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="row">
+          <input className="input" type="text" />
+        </div>
+        <div className="row">
+          <div className='inner-btn'>
+            <button className="button">C</button>
+            <button className="button">%</button>
+            <button className="button">del</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="inner-btn">
+            <button className="button">7</button>
+            <button className="button">8</button>
+            <button className="button">9</button>
+            <button className="button">*</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="inner-btn">
+            <button className="button">4</button>
+            <button className="button">5</button>
+            <button className="button">6</button>
+            <button className="button">/</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="inner-btn">
+            <button className="button">1</button>
+            <button className="button">2</button>
+            <button className="button">3</button>
+            <button className="button">+</button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="inner-btn">
+            <button className="button">0</button>
+            <button className="button">.</button>
+            <button className="button">=</button>
+            <button className="button">-</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
